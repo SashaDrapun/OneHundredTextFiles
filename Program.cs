196125be9df3dc949e8257a1fc5725d188f42428
Alexander Drapun <dr.sasha2602@mail.ru>
2024-08-22 17:21:11 +0300

    Added use of threads

--- Program.cs
+++ Program.cs
@@ -1,61 +1,64 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 class Program
 {
     static void Main()
     {
-        Random random = new Random();
         char[] latinChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".ToCharArray();
         char[] russianChars = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя".ToCharArray();
 
-        for (int fileIndex = 0; fileIndex < 100; fileIndex++)
+        Parallel.For(0, 100, fileIndex =>
         {
             string fileName = $"file_{fileIndex + 1}.txt";
             using (StreamWriter writer = new StreamWriter(fileName, false, Encoding.UTF8))
             {
                 for (int lineIndex = 0; lineIndex < 100000; lineIndex++)
                 {
-                    string date = GenerateRandomDate(random);
-                    string latinString = GenerateRandomString(random, latinChars, 10);
-                    string russianString = GenerateRandomString(random, russianChars, 10);
-                    int evenNumber = GenerateRandomEvenNumber(random);
-                    double randomDouble = GenerateRandomDouble(random);
+                    string date = GenerateRandomDate();
+                    string latinString = GenerateRandomString(latinChars, 10);
+                    string russianString = GenerateRandomString(russianChars, 10);
+                    int evenNumber = GenerateRandomEvenNumber();
+                    double randomDouble = GenerateRandomDouble();
 
                     string line = $"{date}||{latinString}||{russianString}||{evenNumber}||{randomDouble:F8}||";
                     writer.WriteLine(line);
                 }
             }
-        }
+        });
     }
 
-    static string GenerateRandomDate(Random random)
+    static ThreadLocal<Random> random = new ThreadLocal<Random>(() => new Random());
+
+    static string GenerateRandomDate()
     {
-        DateTime startDate = new DateTime(DateTime.Now.Year - 5, DateTime.Now.Month, DateTime.Now.Day);
+        DateTime startDate = new DateTime(DateTime.Now.Year - 5, 1, 1);
         DateTime endDate = DateTime.Now;
         int range = (endDate - startDate).Days;
-        DateTime randomDate = startDate.AddDays(random.Next(range));
+        DateTime randomDate = startDate.AddDays(random.Value.Next(range));
         return randomDate.ToString("dd.MM.yyyy");
     }
 
-    static string GenerateRandomString(Random random, char[] charSet, int length)
+    static string GenerateRandomString(char[] charSet, int length)
     {
         StringBuilder sb = new StringBuilder(length);
         for (int i = 0; i < length; i++)
         {
-            sb.Append(charSet[random.Next(charSet.Length)]);
+            sb.Append(charSet[random.Value.Next(charSet.Length)]);
         }
         return sb.ToString();
     }
 
-    static int GenerateRandomEvenNumber(Random random)
+    static int GenerateRandomEvenNumber()
     {
-        return random.Next(1, 50000000) * 2;
+        return random.Value.Next(1, 50000000) * 2;
     }
 
-    static double GenerateRandomDouble(Random random)
+    static double GenerateRandomDouble()
     {
-        return random.NextDouble() * 19 + 1;
+        return random.Value.NextDouble() * 19 + 1;
     }
 }
